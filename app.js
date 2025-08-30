5297c86374b2dccb39247483fc3d134241a94c63
sueopazo <sueopazo@gmail.com>
2025-08-30 14:08:21 -0400

    implementing agregarAmigo function

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 // Crear un array para almacenar los nombres
+
+
+// Implementa una función para agregar amigos
+
+
+const inputNombres = document.getElementById('amigo');
 let amigosIngresados = [];
+
+function limpiarInput(){
+    inputNombres.value = "";
+}
+
+function agregarAmigo() {
+    let nombreIngresado = inputNombres.value;
+    console.log(nombreIngresado)
+    if(nombreIngresado.length == 0) {
+        alert('Por favor, inserte un nombre.')
+    } else {
+        amigosIngresados.push(nombreIngresado);
+        
+    }
+    console.log(amigosIngresados)
+    limpiarInput();
+}
+
+
